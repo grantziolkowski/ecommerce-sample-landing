5b992fa493073dad2bd7a7ab63888c9284e4c625
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-08 18:19:14 -0400

    Add carousel js functionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  function carousel() {
+   var photo = $('.top_slide.active');
+   var next = photo.next();
+       $('.top_slide').animate({'left' : '-100%'},'slow', function(){
+          photo.removeClass('active');
+          next.addClass('active');
+        })
+      })
+    }
 
+    setInterval(carousel,1000);
 })
