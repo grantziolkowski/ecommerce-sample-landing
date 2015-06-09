a0cabce670f1ef33367014795ea3b8a26290c746
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-09 09:28:54 -0400

    Carousel with correct counter on photo

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,33 +1,31 @@
 $(document).ready(function(){
-  console.log("doc")
+  $(".count").text("1");
   function carousel() {
     var $current = $('#slides li.active')
     $current.removeClass('active')
     if ($current.is($('#slides li').last())) {
-
-      console.log("last")
-        $(".count").text("1");
-        $('#slides li').first().fadeIn();
+        $('#slides').css({'left': '100%'})
         $('#slides li').first().addClass('active')
     }
     // else, hide current slide and show the next one
     else {
-      console.log("not last")
-        $(".count").text($current.index()+1);
         $('#slides').animate({'left': '-100%'},'slow')
         $current.next().addClass('active')
         $current = $current.next();
-      }
     }
-    var totalSlides = $('#slides').children().length
-    setInterval(carousel,2000);
-    $('#total').html(totalSlides)
-    $('#counter').appendTo($('#slides'))
+    $(".count").text($current.index()+1);
+    $('#counter').appendTo($current)
+  }
 
-    $('.bottom_slide.active p').html($('#summary1').text())
-    $('.bottom_description').html($('.description #item1').text())
+  var totalSlides = $('#slides').children().length
+  setInterval(carousel,2000);
+  $('#total').html(totalSlides)
 
-    $('.tabs').children().on('click', function(event){
+
+  $('.bottom_slide.active p').html($('#summary1').text())
+  $('.bottom_description').html($('.description #item1').text())
+
+  $('.tabs').children().on('click', function(event){
       event.preventDefault();
 
       $('.tabs').children().removeClass('active')
@@ -38,7 +36,5 @@ $(document).ready(function(){
       $('.bottom_slide.active p').html($('#summary1').text())
       $('.bottom_description').fadeOut().fadeIn()
       $('.bottom_description').html($('.description #item1').text())
-    })
-
-    $
+  })
 })
