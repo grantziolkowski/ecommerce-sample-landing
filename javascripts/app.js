d494cb569279e33357f521760d73a502b1e95078
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-09 09:10:16 -0400

    Correct carousel size

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
   console.log("doc")
   function carousel() {
-     $('#slides').animate({'left' : '-100%'},'slow', function(){
-        }
-      )
+    var $current = $('#slides li.active')
+    $current.removeClass('active')
+    if ($current.is($('#slides li').last())) {
+
+      console.log("last")
+        $(".count").text("1");
+        $('#slides li').first().fadeIn();
+        $('#slides li').first().addClass('active')
+    }
+    // else, hide current slide and show the next one
+    else {
+      console.log("not last")
+        $(".count").text($current.index()+1);
+        $('#slides').animate({'left': '-100%'},'slow')
+        $current.next().addClass('active')
+        $current = $current.next();
+      }
     }
     var totalSlides = $('#slides').children().length
     setInterval(carousel,2000);
